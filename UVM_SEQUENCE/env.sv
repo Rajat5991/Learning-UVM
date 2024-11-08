3fb6fdb3cd33233edc5a170a895e8323d8e913e5
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:27:53 -0800

    Update env.sv

--- UVM_SEQUENCE/env.sv
+++ UVM_SEQUENCE/env.sv
@@ -1,1 +1,8 @@
-
+class env extends uvm_env;
+  `uvm_component_utils(env)
+  
+  function new(input string name, input uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+endclass
