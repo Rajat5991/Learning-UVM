2b1fdc6895670cb993f3e2860a7f72661707cd28
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:31:44 -0800

    Update packet_sequence.sv

--- UVM_ENV/packet_sequence.sv
+++ UVM_ENV/packet_sequence.sv
@@ -1,1 +1,26 @@
+`ifndef PKT_SEQ_SV
+`define PKT_SEQ_SV
+`include "packet.sv"
+
+class packet_sequence extends uvm_sequence#(packet);
+   `uvm_object_utils(packet_sequence)
+  
+function new(input string name = "packet_sequence");
+  super.new(name);
+endfunction
+
+virtual task body();
+ // req = packet :: type_id :: create("req");
+  repeat(16)
+    `uvm_do(req);
+    //req.randomize();
+    
+  `uvm_info("TEST_INFO",$sformatf("req.sprint()"),UVM_LOW);
+  `uvm_info("TEST_INFO",$sformatf("%m"),UVM_LOW);
+endtask
+
+
+endclass
+
+`endif
 
