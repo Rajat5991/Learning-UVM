3bc290da719e40454177a57cd0c573bc503722be
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:32:12 -0800

    Update packet.sv

--- UVM_ENV/packet.sv
+++ UVM_ENV/packet.sv
@@ -1,1 +1,22 @@
+`ifndef PKT_SV
+`define PKT_SV
+class packet extends uvm_sequence_item;
+  //`uvm_object_utils(packet)
+  
+  function new(input string name = "packet");
+    super.new(name);
+  endfunction
+  
+  `uvm_object_utils_begin(packet)
+   `uvm_field_int(data_load,UVM_ALL_ON)
+   `uvm_field_array_int(pay_load,UVM_ALL_ON)
+  `uvm_object_utils_end
+  
+  rand bit [7:0] data_load;
+  rand bit [7:0] pay_load[];
+  
+  constraint size_payload{ pay_load.size inside {[1:256]}; }
+  
+endclass
 
+`endif
