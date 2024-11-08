418a26a6d1541abf5f5dc8ed265e4e03fd78078c
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:32:40 -0800

    Update environment.sv

--- UVM_ENV/environment.sv
+++ UVM_ENV/environment.sv
@@ -1,1 +1,27 @@
+`ifndef ENV_SV
+`define ENV_SV
 
+//typedef uvm_sequencer#(packet) packet_sequencer;
+
+class test_environment extends uvm_env;
+  `uvm_component_utils(test_environment)
+  
+  //packet_sequencer pkt_seqr;
+  
+  function new(input string name, input uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  
+  virtual function void build_phase(input uvm_phase phase);
+    super.build_phase(phase);
+  //  pkt_seqr = packet_squencer::type_id::create("pkt_seqr",this);
+  endfunction
+ 
+  
+endclass
+
+`endif
+
+  
+  
