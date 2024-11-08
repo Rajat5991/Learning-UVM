8bf23cfaef3e5a9b9e9552d4c102cf673ff1ecaf
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:28:29 -0800

    Update packet_sequence.sv

--- UVM_SEQUENCE/packet_sequence.sv
+++ UVM_SEQUENCE/packet_sequence.sv
@@ -1,1 +1,10 @@
+`include "packet.sv"
 
+class packet_sequence extends uvm_sequence #(packet);
+  `uvm_object_utils(packet_sequence)
+  
+  function new(input string name = "packet_sequence");
+    super.new(name);
+  endfunction
+  
+endclass
