30e99e8542b91e4c09269eb2b189d48ee25df94b
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:51:55 -0800

    Update packet_sequence.sv

--- Virtual_Interface/packet_sequence.sv
+++ Virtual_Interface/packet_sequence.sv
@@ -1,1 +1,30 @@
+`ifndef PKT_SEQ
+`define PKT_SEQ
 
+`include "packet.sv"
+
+class packet_sequence extends uvm_sequence#(packet);
+  `uvm_object_utils(packet_sequence)
+  
+  int num_packets;
+  
+  function new(input string name = "packet_sequence");
+    super.new(name);
+  endfunction
+  
+  task pre_start();
+    if(!uvm_config_db #(int)::get(null,"get_full_name()","num_pkts",num_packets))
+       `uvm_fatal("PKT_SEQ","Failed to get the num_packets");
+  endtask
+
+  
+  task body();
+    repeat(num_packets)
+      `uvm_do(req);
+  endtask
+  
+endclass
+`endif
+    
+  
+  
