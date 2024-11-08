ff86f065bbb8421023bd29864ec5d25cfbb54620
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:26:20 -0800

    Update testbench.sv

--- UVM_SEQUENCE/testbench.sv
+++ UVM_SEQUENCE/testbench.sv
@@ -1,1 +1,13 @@
-
+// Code your testbench here
+// or browse Examples
+module TB;
+  
+   import uvm_pkg::*;
+  `include "testclass.sv"
+  
+  initial
+     begin
+       run_test("test_base");
+     end
+  
+endmodule
