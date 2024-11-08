afe507ea91b676d1586776cb4e6f12786acc1da4
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:37:00 -0800

    Update driver.sv

--- UVM_AGENT/driver.sv
+++ UVM_AGENT/driver.sv
@@ -1,1 +1,24 @@
+`ifndef DRIVER_SV
+`define DRIVER_SV
 
+`include "packet.sv"
+
+class driver extends uvm_driver#(packet);
+  `uvm_component_utils(driver)
+  
+  function new(input string name, input uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  virtual task run_phase(input uvm_phase phase);
+   forever
+    begin
+      seq_item_port.get_next_item(req);
+      `uvm_info("DRIVER_TEST", req.sprint(),UVM_LOW);
+      seq_item_port.item_done();
+    end
+  endtask
+  
+endclass
+
+`endif
