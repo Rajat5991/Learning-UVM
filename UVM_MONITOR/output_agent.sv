f66fd01b441fd2f9e70463dae5ecda58f85916d0
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:45:47 -0800

    Update output_agent.sv

--- UVM_MONITOR/output_agent.sv
+++ UVM_MONITOR/output_agent.sv
@@ -1,1 +1,25 @@
+`ifndef OUT_AGNT
+`define OUT_AGNT
+
+`include "output_monitor.sv"
+
+
+class output_agent extends uvm_agent;
+  `uvm_component_utils(output_agent)
+  
+  function new(input string name, input uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  output_monitor mon;
+  
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    mon = output_monitor :: type_id :: create("mon",this);
+  endfunction
+  
+endclass
+
+`endif
 
