ec0f7de3877e4780f1214c0288d75acdf7953eb2
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:49:25 -0800

    Update interface.sv

--- Virtual_Interface/interface.sv
+++ Virtual_Interface/interface.sv
@@ -1,1 +1,22 @@
-
+`ifndef INTFACE
+`define INTFACE
+interface switch_interface(input bit clk);
+  logic [47:0] src_addr;
+  logic [31:0] src_data;
+  logic [47:0] dst_addr;
+  logic [31:0] dst_data;
+  
+  modport switch_port(input clk, src_addr, src_data,
+                      output dst_addr, dst_data);
+  
+  
+  clocking cb @(posedge clk);
+    default input #2 output #1;
+    output    src_addr, src_data;  
+    input    dst_addr,dst_data;
+  endclocking
+    
+  modport testcase_port(input clk, clocking cb);
+  
+endinterface
+`endif
