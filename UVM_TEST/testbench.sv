51600344280d1ddcd1f19ababbe6acefb4f44e5c
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:22:05 -0800

    Update testbench.sv

--- UVM_TEST/testbench.sv
+++ UVM_TEST/testbench.sv
@@ -1,1 +1,16 @@
+// Code your testbench here
+// or browse Examples
+//import uvm_pkg::*;
+
+program testbench();
+  
+ import uvm_pkg::*;
+`include "testcase.sv";
+  
+  initial
+     begin     
+       run_test("hello_world");
+     end
+  
+endprogram
 
