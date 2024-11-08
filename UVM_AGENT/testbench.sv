adfaa8713683f03924ca00d518a3b4f6f3b98377
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:34:21 -0800

    Update testbench.sv

--- UVM_AGENT/testbench.sv
+++ UVM_AGENT/testbench.sv
@@ -1,1 +1,13 @@
-
+// Code your testbench here
+// or browse Examples
+module TB;
+  
+  import uvm_pkg::*;
+  `include "testclass.sv"
+  
+  initial
+    begin
+      run_test("base_test");
+    end
+endmodule
+  
