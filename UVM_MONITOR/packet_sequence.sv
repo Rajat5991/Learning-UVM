db9af2537983919500d040bac927f460f5cae8fa
Rajat5991 <154459536+Rajat5991@users.noreply.github.com>
2024-11-07 16:43:08 -0800

    Update packet_sequence.sv

--- UVM_MONITOR/packet_sequence.sv
+++ UVM_MONITOR/packet_sequence.sv
@@ -1,1 +1,31 @@
+`ifndef PKT_SEQ
+`define PKT_SEQ
 
+`include "packet.sv"
+
+class packet_sequence extends uvm_sequence#(packet);
+  `uvm_object_utils(packet_sequence)
+  
+  int num_pkt;
+  
+  function new(input string name = "packet_sequence");
+    super.new(name);
+  endfunction
+  
+  virtual task pre_start();
+    `uvm_info("PKT_SEQ",$sformatf("WE ARE IN SEQ PRE START"),UVM_MEDIUM);
+    if(!uvm_config_db #(int) :: get(null,"","num_pkt",num_pkt))
+      `uvm_fatal("SEQ_ERROR","CANNOT GET THE NUM PKT");
+  endtask
+  
+  virtual task body();
+    repeat(num_pkt)
+    `uvm_do(req);
+  endtask
+  
+endclass
+
+`endif
+    
+  
+  
